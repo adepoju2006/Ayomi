3492da7c8685ead0880ca477cb5047d78aa52aa9
adepoju2006 <adepojuayomide2006@gmail.com>
2021-05-12 14:46:21 -0700

    first commit

---
+++ Ayo.js
@@ -0,0 +1,6 @@
+// Using For loop to create your times Table
+
+for (var num = 1; num <= 12; num++) {
+  var multiply = 2 * num;
+  console.log("2 X " + num + " = " + multiply);
+}
---
+++ Bus.js
@@ -0,0 +1,9 @@
+var entry = Number(prompt("How Much Do you Have"));
+
+if (entry > 100) {
+  alert("Enter Bike");
+} else if (entry === 100) {
+  alert("Enter Bus");
+} else {
+  alert("Trek!!!!");
+}
